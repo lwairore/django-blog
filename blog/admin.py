88390c5f35a3c927a715923315cbbc9dd8c47d91
lwairore <kwairore@gmail.com>
2020-04-07 11:20:33 +0300

    - Add helpful comments

--- blog/admin.py
+++ blog/admin.py
@@ -15,11 +15,24 @@ class PostAdmin(admin.ModelAdmin):
 
         The `@admin.register()` decorator performs the same function as the 
         `admin.site.register()` function, registering the `ModelAdmin` class that it decorates.
+
+        The `search_fields` attribute will allow a search bar to appear.
+        Below the search bar, there will be navigation links to navigate through a date hierarchy:
+        this has been defined by the `date_hierarchy` attribute.
+
+        Posts will be ordered by Status and Publish columns by default. We have specified the default order using the `ordering` attribute.
+
+        When Adding Post link as yo type the title of a new post, the `slug` field
+        is filled in automatically. We have told Django to prepopulate the `slug` field with the input of the `titile`
+        field using the `prepopulated_fields` attribute. 
+
+        The `author` field is displayed with a lookup widget that can scale much better than a drop-down select input when you have 1000s of
+        users.
     """
 
-    list_display = ('title', 'slug', 'author', 'publish', 'status')
-    search_fields = ('title', 'body')
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author',)
-    date_hierarchy = 'publish'
+    list_filter = ('status', 'created', 'publish', 'author')    
+    search_fields = ('title', 'body')    
+    prepopulated_fields = {'slug': ('title',)}    
+    raw_id_fields = ('author',)    
+    date_hierarchy = 'publish'    
     ordering = ('status', 'publish')
