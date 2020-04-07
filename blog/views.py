6aa60a4b2d1b4d1ccbece5510f0d68000424eba2
lwairore <kwairore@gmail.com>
2020-04-07 13:00:11 +0300

    - Use class-based views PostListView.

--- blog/views.py
+++ blog/views.py
@@ -2,37 +2,44 @@ from django.shortcuts import render, get_object_or_404
 from . import models
 from django.core.paginator import Paginator, EmptyPage, \
         PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
-def post_list(request):
-    """
-        The `post_list` view takes the `request` object as the only parameter.
-        In this view, we are retrieving all the posts with the `published` status using the `published` manager
-        we created earlier.
+class PostListView(ListView):
+    queryset = models.Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-        Finally, we are using the `render()` shortccut provided by Django to render the list of
-        posts with the given template. This function takes:
-            1. the `request` object,
-            2. the template path,
-            3. and the context variables to render the given template.
-        It returns an `HttpResponse` object wiht the rendered text.
-    """
-    object_list = models.Post.published.all()
-    paginator = Paginator(object_list, 3) # 3 posts in each page
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+# def post_list(request):
+#     """
+#         The `post_list` view takes the `request` object as the only parameter.
+#         In this view, we are retrieving all the posts with the `published` status using the `published` manager
+#         we created earlier.
 
-    return render(request,
-                  'blog/post/list.html',
-                  {'page': page,
-                   'posts': posts})
+#         Finally, we are using the `render()` shortccut provided by Django to render the list of
+#         posts with the given template. This function takes:
+#             1. the `request` object,
+#             2. the template path,
+#             3. and the context variables to render the given template.
+#         It returns an `HttpResponse` object wiht the rendered text.
+#     """
+#     object_list = models.Post.published.all()
+#     paginator = Paginator(object_list, 3) # 3 posts in each page
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # If page is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
+
+#     return render(request,
+#                   'blog/post/list.html',
+#                   {'page': page,
+#                    'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
