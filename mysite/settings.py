50978fd09e081e69a90c42a4e76db0f354cf9a69
lwairore <kwairore@gmail.com>
2020-04-07 10:41:46 +0300

    - Configure static and media files.

--- mysite/settings.py
+++ mysite/settings.py
@@ -134,5 +134,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+SITE_ID = 1
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
