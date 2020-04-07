c4c0897697a82e9c775a61becb9891e29671a9c9
lwairore <kwairore@gmail.com>
2020-04-07 12:49:19 +0300

    - Comment outside urlpatterns.

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,7 @@ from django.conf.urls.static import static
 
 app_name = 'blog'
 
-urlpatterns = [
-    """
+"""
         We use angle brackets to capture the values from the URL.
         Any value specified in the URL pattern as `<parameter>` is captured as a string.
         We use path converters, such as `<int:year>` to specifically match and return
@@ -17,6 +16,8 @@ urlpatterns = [
         If using `path()` and converters isn't sufficient for you, you can use `re_path()` instead to define complex URL patterns with Python regular expressions. You can learn more about defining URL patterns with regular expressions at https://docs.djangoproject.com/en/2.0/ref/urls/#django.urls.re_path.
 
     """
+urlpatterns = [
+    
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
 
