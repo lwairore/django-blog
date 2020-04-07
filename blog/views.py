51441c6bb09ff628fa5f44fe911d5b022ddce0fa
lwairore <kwairore@gmail.com>
2020-04-07 17:47:49 +0300

    - Pass new_form to template.

--- blog/views.py
+++ blog/views.py
@@ -44,29 +44,40 @@ class PostListView(ListView):
 #                    'posts': posts})
 
 
-def post_detail(request, year, month, day, post):
-    """
-        This view takes `year`, `month`, `day`, and `post` parameters to retrieve a published
-        post with the given slug and date. 
-    """
-    post = get_object_or_404(models.Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
 
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(models.Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    
     # List of active comments for this post
     comments = post.comments.filter(active=True)
 
-    new_comment = None:
-    
+    new_comment = None
+
     if request.method == 'POST':
         # A comment was posted
-        comment_from = forms.CommentForm(data=request.POST)
-        if comment_from.is_valid():
+        comment_form = forms.CommentForm(data=request.POST)
+        if comment_form.is_valid():
             # Create Comment object but don't save to database yet
-            new_comment = comment_from.save(commit=False)
+            new_comment = comment_form.save(commit=False)
             # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
             new_comment.save()
     else:
-        comment_from = forms.CommentForm()
-    return render(request, 'blog/post/detail.html', { 'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_from': comment_from })
+        comment_form = forms.CommentForm()
+
+    
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,})
 
 
 def post_share(request, post_id):
