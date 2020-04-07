b2474b7ff8205efbdcecab1725acdef492e6b949
lwairore <kwairore@gmail.com>
2020-04-07 13:02:25 +0300

    - Create '' urlconf to urlpatterns, that is mapped to PostListView.

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,8 @@ app_name = 'blog'
 
     """
 urlpatterns = [
-    
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    # path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail')
 
 ]
