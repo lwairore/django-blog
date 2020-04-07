57dc60dc5c197ea936fcf5ccdf2c7ede17652f17
lwairore <kwairore@gmail.com>
2020-04-07 12:28:18 +0300

    - Set app_name to 'blog'

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+app_name = 'blog'
+
 urlpatterns = [
     """
         We use angle brackets to capture the values from the URL.
