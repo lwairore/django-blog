043b0cde672c83d9fd160af8c29b5646fa92ba95
lwairore <kwairore@gmail.com>
2020-04-07 13:09:54 +0300

    - Rename EmailPostFor to EmailPostForm.

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class EmailPostFor(forms.Form):
+class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
