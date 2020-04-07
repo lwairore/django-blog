218a2b94b33488066466848bf6ba1637a0f5bd41
lwairore <kwairore@gmail.com>
2020-04-07 10:38:52 +0300

    - Configure postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,8 +89,12 @@ CORS_ORIGIN_WHITELIST = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'PORT': '',
+        'NAME': 'blog',
+        'USER': 'karangu',
+        'PASSWORD': 'lmzongolo8754',
+        'HOST': '127.0.0.1'
     }
 }
 
